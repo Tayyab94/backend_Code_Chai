feee88d14745c1f8f62f7891f70330c15f369098
Tayyab94 <tayyab.bhatti30@gmail.com>
2023-11-07 02:19:47 +0500

    write some refernce comments

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
+dotenv.config({
+    path: "./env"
+})
 
 const app = express();
 
@@ -10,9 +14,16 @@ app.use(cors({
     credentials: true
 }));
 
+// this line is to allow the json data with limit
 app.use(express.json({ limit: "16kb" }));
+
+// this is using to encode the Url
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
+// this code is use to read of write the data from public (static folder within the project folder)
 app.use(express.static("public"))
+
+// this middleware will parse cookies and populate req.cookies
 app.use(cookieParser())
 
 export { app }
